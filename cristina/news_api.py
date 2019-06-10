e8b35408d2e45b7c541e7e71e92265590020b77a
Ivditi Gabeskiria <ivg4001@med.cornell.edu>
2019-06-10 17:50:27 -0400

    modify sortby

--- cristina/news_api.py
+++ cristina/news_api.py
@@ -3,6 +3,7 @@ from newsapi import NewsApiClient
 import requests
 import json
 import asyncio
+import datetime
 
 #Init
 newsapi = NewsApiClient(api_key=news_api_key)
@@ -11,6 +12,9 @@ all_articles = newsapi.get_everything(q='tesla',
                                       domains='bbc.co.uk',
                                       language='en',
                                       page=2)
-url = (f'https://newsapi.org/v2/everything?q=bitcoin&from=2019-04-05&sortBy=publishedAt&apiKey={news_api_key}')
+from_date = datetime.date.fromordinal(datetime.date.today().toordinal()-1).strftime("%F")
+to_date = datetime.datetime.today().strftime('%d-%m-%Y')
+
+url = (f'https://newsapi.org/v2/everything?q=bitcoin&from=2019-04-05&sortBy=popularity&apiKey={news_api_key}')
 response = requests.get(url)
 response_json = response.json()
